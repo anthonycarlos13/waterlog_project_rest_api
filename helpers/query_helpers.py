b6f079ddb64872acda84b0cec4878a40e1e70ec8
Anthony Mendoza <mendoza_ant@berkeley.edu>
2016-06-09 19:24:26 -0700

    improving dynamic field lookups

--- helpers/query_helpers.py
+++ helpers/query_helpers.py
@@ -7,7 +7,11 @@ from rest_framework import status
 def dynamic_field_lookups(query_params):
     Qr = None
     for filter_by, filter_value in query_params.iteritems():
-        q = Q(**{"%s" % filter_by: filter_value})
+        if filter_by == 'dam_id':
+            q = Q(**{"%s__iexact" % filter_by: filter_value})
+        else:
+            q = Q(**{"%s" % filter_by: filter_value})
+
         if Qr:
             Qr = Qr & q
         else:
@@ -15,40 +19,3 @@ def dynamic_field_lookups(query_params):
     return Qr
 
 
-def type_of_query_param(param, obj_name, filter_by):
-    try:
-        param = int(param)
-        obj_list = obj_name.objects.filter(dam_id__iexact=param)
-    except ValueError:
-        Qr = Q(**{"%s" % filter_by: param})
-        obj_list = obj_name.objects.filter(Qr)
-    return obj_list
-
-
-def get_products(obj_name, obj_serializer, request):
-    data = obj_name.objects.all()
-    serializer = obj_serializer(data, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-def update(obj_name, obj_serializer, request):
-    Qr = dynamic_field_lookups(request.query_params)
-    try:
-        obj = obj_name.objects.get(Qr)
-    except:
-        raise ValueError("cannot make a PUT request to multiple products! Filter by 'id' or 'model_number' instead.")
-    serializer = obj_serializer(obj, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-def delete(obj_name, request):
-    Qr = dynamic_field_lookups(request.query_params)
-    try:
-        obj = obj_name.objects.get(Qr)
-    except:
-        raise ValueError("cannot make a Delete request to multiple products! Filter by 'id' or 'model_number' instead.")
-    obj.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
--- water_store/api_register/reservoir_api.py
+++ water_store/api_register/reservoir_api.py
@@ -28,38 +28,36 @@ station_id_inventory = ['ORO', 'CLE', 'LEW', 'WHI', 'ANT',
 @api_view(['GET'])
 def reservoir_api(request, format=None, **kwargs):
     if request.method == 'GET':
-        id_filter = request.query_params['dam_id']
         try:
-            if id_filter.lower() in map(lambda x:x.lower(),station_id_inventory):
-                Qr = query_helpers.dynamic_field_lookups(request.query_params)
-                reservoirs = reservoir_model.objects.filter(Qr)
-                if reservoirs.count() < 1:
-                    data = aggregate_scraped_data()
-                    for i in data.values():
-                        for j in i:
-                            if j['date'] is not None:
-                                input_model = reservoir_model(dam_id=str(j['id']),
-                                                              name=str(j['name']),
-                                                              reservoir_elevation=None if j['reservoir elevation'] is None or j['reservoir elevation']=='--' else float(''.join(y for y in j['reservoir elevation'] if y.isdigit() or y == '.')),
-                                                              reservoir_area=j['reservoir_area'] if type(j['reservoir_area']) is float else float(''.join(y for y in j['reservoir_area'] if y.isdigit() or y == '.')),
-                                                              reservoir_storage=None if j['reservoir storage'] is None or j['reservoir storage']=='--' else float(''.join(y for y in j['reservoir storage'] if y.isdigit() or y == '.')),
-                                                              latitude=j['latitude'],
-                                                              longitude=j['longitude'],
-                                                              county=str(j['county']),
-                                                              stream=str(j['stream']),
-                                                              storage_capacity=None if j['storage_capacity'] is None else float(''.join(y for y in j['storage_capacity'] if y.isdigit() or y == '.')),
-                                                              outflow=None if j['outflow'] is None or j['outflow']=='--' else float(''.join(y for y in j['outflow'] if y.isdigit() or y == '.')),
-                                                              inflow=None if j['inflow'] is None or j['inflow']=='--' else float(''.join(y for y in j['inflow'] if y.isdigit() or y == '.' or y == ',')),
-                                                              precipitation_incremental=None if j['precipitation incremental'] is None or j['precipitation incremental']=='--'else float(''.join(y for y in j['precipitation incremental'] if y.isdigit() or y == '.')),
-                                                              precipitation_accumulated=None if j['precipitation accumulated'] is None or j['precipitation accumulated']=='--'else float(''.join(y for y in j['precipitation accumulated'] if y.isdigit() or y == '.')),
-                                                              date=datetime.strptime(j['date'], '%m/%d/%Y'),
-                                                              source="State Water Project")
-                                input_model.save()
-                            else:
-                                continue
-                _serializer = ReservoirSerializer(reservoirs, many=True)
-                return Response(_serializer.data, status=status.HTTP_200_OK)
-            else:
-                raise Response(KeyError('dam id not found'), status=status.HTTP_400_BAD_REQUEST)
+            Qr = query_helpers.dynamic_field_lookups(request.query_params)
+            print Qr
+            reservoirs = reservoir_model.objects.filter(Qr)
+            dam_id = request.query_params['dam_id']
+            if dam_id.lower() in map(lambda x:x.lower(),station_id_inventory) and reservoirs.count() < 1:
+                data = aggregate_scraped_data()
+                for i in data.values():
+                    for j in i:
+                        if j['date'] is not None:
+                            input_model = reservoir_model(dam_id=str(j['id']),
+                                                          name=str(j['name']),
+                                                          reservoir_elevation=None if j['reservoir elevation'] is None or j['reservoir elevation']=='--' else float(''.join(y for y in j['reservoir elevation'] if y.isdigit() or y == '.')),
+                                                          reservoir_area=j['reservoir_area'] if type(j['reservoir_area']) is float else float(''.join(y for y in j['reservoir_area'] if y.isdigit() or y == '.')),
+                                                          reservoir_storage=None if j['reservoir storage'] is None or j['reservoir storage']=='--' else float(''.join(y for y in j['reservoir storage'] if y.isdigit() or y == '.')),
+                                                          latitude=j['latitude'],
+                                                          longitude=j['longitude'],
+                                                          county=str(j['county']),
+                                                          stream=str(j['stream']),
+                                                          storage_capacity=None if j['storage_capacity'] is None else float(''.join(y for y in j['storage_capacity'] if y.isdigit() or y == '.')),
+                                                          outflow=None if j['outflow'] is None or j['outflow']=='--' else float(''.join(y for y in j['outflow'] if y.isdigit() or y == '.')),
+                                                          inflow=None if j['inflow'] is None or j['inflow']=='--' else float(''.join(y for y in j['inflow'] if y.isdigit() or y == '.' or y == ',')),
+                                                          precipitation_incremental=None if j['precipitation incremental'] is None or j['precipitation incremental']=='--'else float(''.join(y for y in j['precipitation incremental'] if y.isdigit() or y == '.')),
+                                                          precipitation_accumulated=None if j['precipitation accumulated'] is None or j['precipitation accumulated']=='--'else float(''.join(y for y in j['precipitation accumulated'] if y.isdigit() or y == '.')),
+                                                          date=datetime.strptime(j['date'], '%m/%d/%Y'),
+                                                          source="State Water Project")
+                            input_model.save()
+                        else:
+                            continue
+            _serializer = ReservoirSerializer(reservoirs, many=True)
+            return Response(_serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response(e.message, status=status.HTTP_400_BAD_REQUEST)
